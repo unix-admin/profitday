d74824d96484ac0edd09878caf6cc2c74a059392
DreadThar <s.sandul.vodolei@gmail.com>
2016-01-04 11:22:26 +0200

    add content form

--- models/Content.php
+++ models/Content.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace app\models;
-
+use yii\db\ActiveRecord;
 use Yii;
 
 /**
@@ -19,7 +19,7 @@ use Yii;
  * @property string $tags_en
  * @property string $url
  */
-class Content extends \yii\db\ActiveRecord
+class Content extends ActiveRecord
 {
     /**
      * @inheritdoc
@@ -35,7 +35,7 @@ class Content extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['title_uk', 'title_ru', 'title_en', 'content_uk', 'content_ru', 'content_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'required'],
+            [['title_uk', 'title_ru', 'title_en', 'content_uk', 'content_ru', 'content_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'default'],
             [['content_uk', 'content_ru', 'content_en'], 'string'],
             [['title_uk', 'title_ru', 'title_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'string', 'max' => 255]
         ];
@@ -47,26 +47,17 @@ class Content extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
-            'title_uk' => Yii::t('app', 'Title Uk'),
-            'title_ru' => Yii::t('app', 'Title Ru'),
-            'title_en' => Yii::t('app', 'Title En'),
-            'content_uk' => Yii::t('app', 'Content Uk'),
-            'content_ru' => Yii::t('app', 'Content Ru'),
-            'content_en' => Yii::t('app', 'Content En'),
-            'tags_uk' => Yii::t('app', 'Tags Uk'),
-            'tags_ru' => Yii::t('app', 'Tags Ru'),
-            'tags_en' => Yii::t('app', 'Tags En'),
-            'url' => Yii::t('app', 'Url'),
+            'id' => 'ID',
+            'title_uk' => 'Title Uk',
+            'title_ru' => 'Title Ru',
+            'title_en' => 'Title En',
+            'content_uk' => 'Content Uk',
+            'content_ru' => 'Content Ru',
+            'content_en' => 'Content En',
+            'tags_uk' => 'Tags Uk',
+            'tags_ru' => 'Tags Ru',
+            'tags_en' => 'Tags En',
+            'url' => 'Url',
         ];
-    }
-
-    /**
-     * @inheritdoc
-     * @return ContentQuery the active query used by this AR class.
-     */
-    public static function find()
-    {
-        return new ContentQuery(get_called_class());
     }
 }
---
+++ views/content/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PostSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Contents';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="content-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Content', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            // 'id',
+            'title_uk',
+            //'title_ru',
+            // 'title_en',
+            // 'content_uk:ntext',
+            // 'content_ru:ntext',
+            // 'content_en:ntext',
+            // 'tags_uk',
+            // 'tags_ru',
+            //  'tags_en',
+            'url:url',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>

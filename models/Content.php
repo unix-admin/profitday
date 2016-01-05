819762f822a912e2e15f36951c16cbeb2adb98e9
DreadThar <s.sandul.vodolei@gmail.com>
2016-01-05 13:41:59 +0200

    Revert "add Реализовать предпросмотр измененного контента"
    This reverts commit c241352ae1fd3f39032da39951e39a152f02476f.

--- models/Content.php
+++ models/Content.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace app\models;
-use yii\db\ActiveRecord;
+
 use Yii;
 
 /**
@@ -19,7 +19,7 @@ use Yii;
  * @property string $tags_en
  * @property string $url
  */
-class Content extends ActiveRecord
+class Content extends \yii\db\ActiveRecord
 {
     /**
      * @inheritdoc
@@ -35,7 +35,7 @@ class Content extends ActiveRecord
     public function rules()
     {
         return [
-            [['title_uk', 'title_ru', 'title_en', 'content_uk', 'content_ru', 'content_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'default'],
+            [['title_uk', 'title_ru', 'title_en', 'content_uk', 'content_ru', 'content_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'required'],
             [['content_uk', 'content_ru', 'content_en'], 'string'],
             [['title_uk', 'title_ru', 'title_en', 'tags_uk', 'tags_ru', 'tags_en', 'url'], 'string', 'max' => 255]
         ];
@@ -47,17 +47,26 @@ class Content extends ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'title_uk' => 'Title Uk',
-            'title_ru' => 'Title Ru',
-            'title_en' => 'Title En',
-            'content_uk' => 'Content Uk',
-            'content_ru' => 'Content Ru',
-            'content_en' => 'Content En',
-            'tags_uk' => 'Tags Uk',
-            'tags_ru' => 'Tags Ru',
-            'tags_en' => 'Tags En',
-            'url' => 'Url',
+            'id' => Yii::t('app', 'ID'),
+            'title_uk' => Yii::t('app', 'Title Uk'),
+            'title_ru' => Yii::t('app', 'Title Ru'),
+            'title_en' => Yii::t('app', 'Title En'),
+            'content_uk' => Yii::t('app', 'Content Uk'),
+            'content_ru' => Yii::t('app', 'Content Ru'),
+            'content_en' => Yii::t('app', 'Content En'),
+            'tags_uk' => Yii::t('app', 'Tags Uk'),
+            'tags_ru' => Yii::t('app', 'Tags Ru'),
+            'tags_en' => Yii::t('app', 'Tags En'),
+            'url' => Yii::t('app', 'Url'),
         ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return ContentQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new ContentQuery(get_called_class());
     }
 }
